fc3117ca29fa5657f11fcd7b71f6c981f4db9055
leobiscaino <93631643+leobiscaino@users.noreply.github.com>
2022-02-01 10:50:30 -0400

    Delete Desafio 2.py

--- Desafio 2.py
+++
@@ -1,5 +0,0 @@
-n = float(input('insira o número desejado: '))
-triplo = 3*n
-dobro = 2*n
-raiz=n**0.5
-input('o tripo deste número é {}, o dobro é {} e a raiz deste número é {}'.format(triplo, dobro, raiz))
